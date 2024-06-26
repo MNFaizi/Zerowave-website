66edaff4ace8e5589ea1a050538f106626029cb1
Mochamad NurFaizi <mfaiz1365@gmail.com>
2024-06-26 23:15:20 +0700

    add logout function

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function SideAdmin() {
+    const router = useRouter()
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        router.push('/')
+    }
     return (
         <>
             <aside className="flex flex-col w-68 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
@@ -46,7 +52,7 @@ export default function SideAdmin() {
                             <Image className="object-cover mx-2 rounded-full h-9 w-9" src={'/Zerowave-Crop.png'} alt="avatar" width={60} height={60} />
                             <span className="mx-2 font-medium text-gray-800 dark:text-gray-200 text-xl">Zerowave</span>
                         </a>
-                        <button type="submit">
+                        <button type="submit" onClick={handleLogout}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                             </svg>
