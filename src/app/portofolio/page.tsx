e8a0508bb8ad511d112cbb964641f049a43873f6
Mochamad NurFaizi <mfaiz1365@gmail.com>
2024-06-26 16:23:23 +0700

    page project get from api

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -1,8 +1,8 @@
 import ProjectCard, { ProjectProp } from "@/components/portofolio/Card";
 import { getProject} from "@/lib/utils/getProject";
 
-export default function Portofolio() {
-    const datas = getProject()
+export default async function Portofolio() {
+    const datas = await getProject()
     return (
         <>
             <div className=" container text-center">
@@ -13,7 +13,7 @@ export default function Portofolio() {
                 <div className="flex flex-col justify-center items-center">
                     <h2 className="text-2xl font-sans font-semibold tracking-[4px]">Mainnet</h2>
                     <div className="flex flex-wrap items-center justify-center bg-zero-main bg-opacity-40 rounded-2xl w-[90%] m-4">
-                        {datas.map((data: ProjectProp, index) => {
+                        {datas.map((data: ProjectProp, index : number) => {
                             return (
                                 data.mainnet ?
                                     <div key={data.project_name} className="lg:w-1/4">
@@ -25,10 +25,10 @@ export default function Portofolio() {
                     </div>
                     <h2 className="text-2xl font-sans font-semibold tracking-[4px]">Testnet</h2>
                     <div className="flex flex-wrap bg-zero-main bg-opacity-40 items-center justify-center rounded-2xl w-[90%] m-6">
-                        {datas.map((data: ProjectProp, index) => {
+                        {datas.map((data: ProjectProp, index : number) => {
                             return (
                                 data.mainnet === false ?
-                                    <div className="lg:w-1/4" key={data.name}>
+                                    <div className="lg:w-1/4" key={data.project_name}>
                                         <ProjectCard project={data} index={index} />
                                     </div>
                                     : null
