1769a7ab92ba473dc95a6f3bc56707d5f26948cf
Mochamad NurFaizi <mfaiz1365@gmail.com>
2024-06-27 19:52:21 +0700

    use client side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+"use client"
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import SocialDiv from '@/components/home/SocialDiv'
 import WhyStakeUs from '@/components/home/WhyStake'
 import Image from 'next/image'
 import Link from 'next/link'
-import Portofolio from './portofolio/page'
-import { Suspense } from 'react'
+import { useEffect, useState } from 'react'
+import ProjectCard, { ProjectProp } from "@/components/portofolio/Card";
+import { getProject } from "@/lib/utils/getProject";
 import Loading from './loading'
 
-export const dynamic ="force-dynamic"
-
 export default function Home() {
+  const [datas, setDatas] = useState<ProjectProp []>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProject();
+      setDatas(data);
+      setIsLoading(false);
+    }  
+    fetchData()
+  }, []);
+  if(isLoading) return <Loading/>
   return (
     <>
       <Navbar />
@@ -21,6 +32,7 @@ export default function Home() {
             alt='Zerowave Logo'
             width={275}
             height={275}
+            priority
           />
           <span
             className='w-3/4 lg:w-1/2 text-center my-3 font-semibold text-lg'
@@ -35,10 +47,41 @@ export default function Home() {
           <span className="font-semibold text-3xl my-3 text-zero-secondary">Our Social Media</span>
           <SocialDiv />
         </div>
-        <Suspense fallback={<Loading/>}>
-          <Portofolio />
-        </Suspense>
-      </main>
+        <section id='portofolio'>
+          <div className='container text-center'>
+            <h1 className="text-4xl font-bold my-2">Portofolio Page</h1>
+            <p className='text-xl text-opacity-40'>This is a collection of projects that I supported. You can find the source code and more information</p>
+          </div>
+            <div className="container my-8">
+              <div className="flex flex-col justify-center items-center">
+                <h2 className="text-2xl font-sans font-semibold tracking-[4px]">Mainnet</h2>
+                <div className="flex flex-wrap items-center justify-center bg-zero-main bg-opacity-40 rounded-2xl w-[90%] m-4">
+                  {datas.map((data: ProjectProp, index: number) => {
+                    return (
+                      data.mainnet ?
+                        <div key={data.project_name} className="lg:w-1/4">
+                          <ProjectCard project={data} index={index} />
+                        </div>
+                        : null
+                    )
+                  })}
+                </div>
+                <h2 className="text-2xl font-sans font-semibold tracking-[4px]">Testnet</h2>
+                <div className="flex flex-wrap bg-zero-main bg-opacity-40 items-center justify-center rounded-2xl w-[90%] m-6">
+                  {datas.map((data: ProjectProp, index: number) => {
+                    return (
+                      data.mainnet === false ?
+                        <div className="lg:w-1/4" key={data.project_name}>
+                          <ProjectCard project={data} index={index} />
+                        </div>
+                        : null
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+        </section>
+      </main >
       <Footer />
     </>
   )
