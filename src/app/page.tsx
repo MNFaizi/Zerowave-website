969e083906a4962eedbba008c41ad6a954dcf4f5
Mochamad NurFaizi <mfaiz1365@gmail.com>
2024-06-27 11:58:12 +0700

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 import SocialDiv from '@/components/home/SocialDiv'
 import WhyStakeUs from '@/components/home/WhyStake'
 import Image from 'next/image'
 import Link from 'next/link'
+import Portofolio from './portofolio/page'
+import { Suspense } from 'react'
+import Loading from './loading'
+
+export const dynamic ="force-dynamic"
 
 export default function Home() {
   return (
-    <main className="lg:container my-auto">
+    <>
+      <Navbar />
+      <main className="lg:container my-auto">
         <div className='flex flex-col items-center'>
           <Image
             src={'/Zerowave-Crop.png'}
@@ -14,18 +23,23 @@ export default function Home() {
             height={275}
           />
           <span
-            className='w-3/4 lg:w-1/2 text-center my-2 font-semibold text-lg'
+            className='w-3/4 lg:w-1/2 text-center my-3 font-semibold text-lg'
           >
             Welcome to our cutting-edge Validator Hub, revolutionizing blockchain trust and efficiency through expert node operations. Join us in shaping the blockchain future.
           </span>
-          <Link href='/portofolio' className='bg-zero-main-opposite rounded-md transition m-3 hover:scale-110 hover:bg-zero-secondary'>
+          <Link href='#portofolio' className='bg-zero-main-opposite rounded-md transition m-3 hover:scale-110 hover:bg-zero-secondary'>
             <button className='w-44 h-12 font-bold text-xl' style={{ color: 'white' }}>Stake With Us</button>
           </Link>
           <span className="font-semibold text-3xl text-zero-secondary">Why Stake With Zerowave Validator</span>
           <WhyStakeUs />
-          <span className="font-semibold text-3xl text-zero-secondary">Our Social Media</span>
+          <span className="font-semibold text-3xl my-3 text-zero-secondary">Our Social Media</span>
           <SocialDiv />
         </div>
-    </main>
+        <Suspense fallback={<Loading/>}>
+          <Portofolio />
+        </Suspense>
+      </main>
+      <Footer />
+    </>
   )
 }
